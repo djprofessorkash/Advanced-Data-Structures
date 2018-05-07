169a5fb82b5aa7d3c36a4f2b5a681d9dd024b5e7
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-05-07 16:26:06 -0700

    Altered some minor comments for my sanity

--- source/priorityqueue.py
+++ source/priorityqueue.py
@@ -4,54 +4,52 @@ from binaryheap import BinaryMinHeap
 
 
 class PriorityQueue(object):
-    """PriorityQueue: a partially ordered queue with methods to enqueue items
-    in priority order and to access and dequeue its highest priority item.
-    Item pairs are stored in a binary min heap for its efficient operations."""
+    """ PriorityQueue: a partially ordered queue with methods to enqueue items
+    in priority order and to access and dequeue its highest priority item.\n
+    Item pairs are stored in a binary min heap for its efficient operations. """
 
     def __init__(self):
-        """Initialize this priority queue."""
-        # Initialize new binary min heap to store items in this priority queue
-        self.heap = BinaryMinHeap()
+        """ Initializes priority queue. """
+        self.heap = BinaryMinHeap()         # Initializes new binary min heap to store items in priority queue
 
     def __repr__(self):
-        """Return a string representation of this priority queue."""
-        return 'PriorityQueue({} items, front={})'.format(self.size(), self.front())
+        """ Returns string representation of priority queue. """
+        return "PriorityQueue({} items, front={})".format(self.size(), self.front())
 
     def is_empty(self):
-        """Return True if this priority queue is empty, or False otherwise."""
+        """ Returns True if priority queue is empty, else returns False. """
         return self.heap.is_empty()
 
     def length(self):
-        """Return the number of items in this priority queue."""
+        """ Returns number of items in priority queue. """
         return self.heap.size()
 
     def enqueue(self, item, priority):
-        """Insert the given item into this priority queue in order according to
-        the given priority."""
-        # TODO: Insert given item into heap in order according to given priority
-        # ...
+        """ Inserts given item into priority queue in order according to given priority. """
+        # TODO: Inserts given item into heap in order according to given priority
+        
 
     def front(self):
-        """Return the item at the front of this priority queue without removing
-        it, or None if this priority queue is empty."""
+        """ Returns item at front of priority queue without removing
+        it, or None if priority queue is empty. """
         if self.size() == 0:
             return None
-        # TODO: Return minimum item from heap
+        # TODO: Returns minimum item from heap
         # ...
 
     def dequeue(self):
-        """Remove and return the item at the front of this priority queue,
-        or raise ValueError if this priority queue is empty."""
+        """ Removes and returns item at front of priority queue,
+        or raises ValueError if priority queue is empty. """
         if self.size() == 0:
-            raise ValueError('Priority queue is empty and has no front item')
-        # TODO: Remove and return minimum item from heap
+            raise ValueError("Priority queue is empty and has no front item.")
+        # TODO: Removes and returns minimum item from heap
         # ...
 
     def push_pop(self, item, priority):
-        """Remove and return the item at the front of this priority queue,
-        and insert the given item in order according to the given priority.
+        """ Removes and returns item at front of priority queue,
+        and inserts given item in order according to given priority.\n
         This method is more efficient than calling dequeue and then enqueue."""
         if self.size() == 0:
-            raise ValueError('Priority queue is empty and has no front item')
-        # TODO: Replace and return minimum item from heap
+            raise ValueError("Priority queue is empty and has no front item.")
+        # TODO: Replaces and returns minimum item from heap
         # ...
