80c6a06d2c237da8d4dd9501a3c9865111b5714d
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-05-15 12:32:11 -0700

    Completed basic Trie structure

--- source/trie.py
+++ source/trie.py
@@ -68,50 +68,111 @@ class Trie(object):
         RUNTIME (WORST):    O(?)    ->  ???\t
         MEMORY (OPTIMAL):   O(?)    ->  ???
         """
-        """ Method that adds word by adding/traversing letters in Trie structure. """
-        is_word_complete, current = True, self.head
+        is_word_complete, current_node = True, self.head
 
         for iterator in range(len(word)):
-            if word[iterator] in current.children:
-                current = current.children[word[iterator]]
+            if word[iterator] in current_node.children:
+                current_node = current_node.children[word[iterator]]
             else:
                 is_word_complete = False
                 break
 
         if not is_word_complete:
             while iterator < len(word):
-                current.add_child(word[iterator])
-                current = current.children[word[iterator]]
+                current_node.add_child(word[iterator])
+                current_node = current_node.children[word[iterator]]
                 iterator += 1
         
         # NOTE: Stores full word as data at end of word in Trie for testing purposes
-        current.data = word
+        current_node.data = word
 
     def contains_word(self, word):
-        """ Method that checks if input word is contained as depth-first word in Trie structure. """
+        """ 
+        Method that checks if input word is contained as depth-first word in Trie structure.\n
+        PARAMS: { word }        ->  char(), str()\t
+        RETURN: { does_exist }  ->  bool()\n
+        RUNTIME (BEST):     O(?)    ->  ???\t
+        RUNTIME (WORST):    O(?)    ->  ???\t
+        MEMORY (OPTIMAL):   O(?)    ->  ???
+        """
         if len(word) < 1:
             return False
         if word is None:
             raise ValueError("Trie.contains_word() requires a non-empty string.")
 
-        current, does_exist = self.head, True
+        current_node, does_exist = self.head, True
 
         for letter in word:
-            if letter in current.children:
-                current = current.children[letter]
+            if letter in current_node.children:
+                current_node = current_node.children[letter]
             else:
                 does_exist = False
                 break
 
         if does_exist:
-            if current.data == None:
+            if current_node.data == None:
                 does_exist = False
-                
+
         return does_exist
 
-
     def starts_with_prefix(self, prefix):
-        pass
+        """ 
+        Method that checks which words contained in Trie structure start with input prefix.\n
+        PARAMS: { prefix }          ->  char(), str()\t
+        RETURN: { prefixed_words }  ->  list()\n
+        RUNTIME (BEST):     O(?)    ->  ???\t
+        RUNTIME (WORST):    O(?)    ->  ???\t
+        MEMORY (OPTIMAL):   O(?)    ->  ???
+        """
+        prefixed_words = list()
+
+        if prefix is None:
+            raise ValueError("Prefix must not be null/None.")
+
+        top_node = self.head
+        for letter in prefix:
+            if letter in top_node.children:
+                top_node = top_node.children[letter]
+            else:
+                return prefixed_words
+        
+        if top_node == self.head:
+            prefixes_queue = [node for key, node in top_node.children.items()]
+        else:
+            prefixes_queue = [top_node]
+
+        while prefixes_queue:
+            current_node = prefixes_queue.pop()
+            if current_node.data is not None:
+                prefixed_words.append(current_node.data)
+            prefixes_queue = [node for key, node in current_node.children.items()] + prefixes_queue
+
+        return prefixed_words
+
 
     def get_data_by_word(self, word):
-        pass
+        """ 
+        Method that grabs and returns data contained by final node in word.\n
+        PARAMS: { prefix }          ->  char(), str()\t
+        RETURN: { prefixed_words }  ->  list()\n
+        RUNTIME (BEST):     O(?)    ->  ???\t
+        RUNTIME (WORST):    O(?)    ->  ???\t
+        MEMORY (OPTIMAL):   O(?)    ->  ???
+        """
+        if not self.contains_word(word):
+            raise ValueError("{} not found in Trie.".format(word))
+
+        current_node = self.head
+        for letter in word:
+            current_node = current_node[letter]
+
+        return current_node.data
+
+if __name__ == "__main__":
+    trie = Trie()
+    words = "hello goodbye help gerald gold tea ted team to too tom stan standard money"
+
+    for word in words.split():
+        trie.add_word(word)
+
+    print("'Goodbye' in Trie: {}".format(trie.contains_word("Goodbye")))
